57e1d990d8523f43d784773a5851ad2264929eed
h0lg3rs <100579956+h0lg3rs@users.noreply.github.com>
2022-03-24 13:27:16 +0100

    Laget en test fil ved bruk av Mockito

---
+++ src/test/java/inf112/skeleton/app/PlayerTest.java
@@ -0,0 +1,40 @@
+package inf112.skeleton.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PlayerTest {
+    
+    @Mock
+    Player p;
+
+	@BeforeAll
+	public static void setUpBeforeClass() throws Exception {
+	}
+	
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCanJump() {
+		assertFalse(p.GetCanJump());
+	}
+	
+	@Test
+	public void testInBounds() {
+		assertFalse(p.inBounds());
+	}
+	
+	@Test
+	public void testHealth() {
+		assertEquals(100, p.getHealth());
+	}
+}
